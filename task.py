7f6ebd1497315bcb734dbbd6919fc4843a488194
MaksimChislavskiy <maxwell884884@gmail.com>
2025-05-19 23:45:54 +0300

    add class and func

---
+++ task.py
@@ -0,0 +1,20 @@
+# Менеджер задач
+#
+# Задача: Создай класс Task, который позволяет управлять задачами (делами).
+# У задачи должны быть атрибуты: описание задачи, срок выполнения и статус (выполнено/не выполнено).
+# Реализуй функцию для добавления задач, отметки выполненных задач и вывода списка текущих (не выполненных) задач.
+
+class Task():
+    def __init__(self, task_description, due_date, status=False):
+        self.task_description = task_description
+        self.due_date = due_date
+        self.status = status
+
+def add_task(self, task):
+    pass
+
+def task_completion(self, task):
+    pass
+
+def info(self):
+    pass
