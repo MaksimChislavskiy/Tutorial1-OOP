e09c5c6b3c6059a7d36028ed0bac4705cd1a3397
MaksimChislavskiy <maxwell884884@gmail.com>
2025-05-20 01:04:44 +0300

    final task

--- task.py
+++ task.py
@@ -10,11 +10,42 @@ class Task():
         self.due_date = due_date
         self.status = status
 
-def add_task(self, task):
-    pass
+    def __repr__(self):
+        return f'{self.task_description}, {self.due_date}'
 
-def task_completion(self, task):
-    pass
+tasks = []
 
-def info(self):
-    pass
+def add_task(task_description, due_date):
+    tasks.append(Task(task_description, due_date))
+    print(f'Добавлена задача: {task_description}, Срок: {due_date}')
+
+def task_completion(task_description):
+    for task in tasks:
+        if task.task_description == task_description:
+            task.status = True
+    print(f'Задача {task_description} выполнена.')
+
+def info():
+    current = []
+    all_competed = True
+    for task in tasks:
+        if not task.status:
+            current.append(task)
+            all_competed = False
+
+    if all_competed:
+        print('Все задачи выполнены')
+    else:
+        print(f'Не выполненные задачи: {current}')
+
+
+# Пример использования
+if __name__ == "__main__":
+    add_task('Позвонить маме', '22.05.2025')
+    add_task('Купить хлеб', '20.05.2025')
+
+    info()
+
+    task_completion('Купить хлеб')
+
+    info()
